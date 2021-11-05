05df23c20517b99293d8d86dfd12ff7cb7a47779
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-11-05 13:49:41 +0000

    👔 rendering list and some business logic
    checked

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+import { Loading } from "./Loading";
+import { ResultsData } from "../types/data-type";
 import { useGlobalContext } from "../context/GlobalContext";
 
 export const Results: React.FC = () => {
@@ -9,20 +11,31 @@ export const Results: React.FC = () => {
 
   const location = useLocation();
 
-  console.log({
-    results,
-    loading,
-  });
-
   useEffect(() => {
     fetchResults("/search/q=JavaScript Mastery&num=40");
   }, [fetchResults]);
+
+  if (loading) return <Loading />;
 
   switch (location.pathname) {
     case "/search":
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-          <h1>Search Results</h1>
+          {results?.results?.map((item: ResultsData, index: number) => {
+            const { link, title } = item;
+            return (
+              <div key={index} className="md:w-2/5 w-full">
+                <a href={link} target="_blank" rel="noreferrer">
+                  <p className="text-sm">
+                    {link.length > 30 ? link.substring(0, 30) : link}
+                  </p>
+                  <p className="text-lg hover:underline dark:text-blue-300 text-blue-700  ">
+                    {title}
+                  </p>
+                </a>
+              </div>
+            );
+          })}
         </div>
       );
 
@@ -33,7 +46,7 @@ export const Results: React.FC = () => {
         </div>
       );
 
-    case "/search":
+    case "/news":
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
           <h1>News Result</h1>
